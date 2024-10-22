204635a67bc6f2a8c5a9e68ccee458568c233d25
Wanderson Andrade <dev@wandersonandrade.com>
2024-10-22 10:21:30 -0300

    cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "vue-nuxt-cypress",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
